7b815dc49dac194db7239bd35d1d273b91f79345
lixmk <lixmk@protonmail.com>
2016-03-29 12:44:28 -0600

    Update int-auto-recon.sh

--- int-auto-recon.sh
+++ int-auto-recon.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Mike Kelly
 #@lixmk
-
+#	TODO: Check dependancies
+#	TODO: Install missing dependancies
+#	TODO: Make sure I'm spelling dependancies correctly
 echo " "
 echo "###################################################################################"
 echo "# int-auto-recon.sh performs recon, enumeration, and vulnerability identification #"
@@ -13,7 +15,8 @@ echo "#             WARNING: This is NOT QUIET. In fact, it's FUCKING LOUD.     
 echo "#             WARNING: This is NOT QUIET. In fact, it's FUCKING LOUD.             #"
 echo "#             WARNING: This is NOT QUIET. In fact, it's FUCKING LOUD.             #"
 echo "#                                                                                 #"
-echo "#        Please check the readme for a list of tools used and dependacies.        #"
+echo "#        Please check the readme for a list of tools used and dependecies.        #"
+echo "#                              # TODO: create readme                              #"
 echo "###################################################################################"
 echo " "
 
@@ -33,13 +36,11 @@ mkdir ./int-auto-recon/nikto
 mkdir ./int-auto-recon/initial
 mkdir ./int-auto-recon/medusa
 mkdir ./int-auto-recon/ssh-ciphers
-mkdir ./int-auto-recon/sslciphers
+mkdir ./int-auto-recon/ssl-ciphers
 mkdir ./int-auto-recon/robots
 mkdir ./int-auto-recon/enum4linux
 mkdir ./int-auto-recon/enum4linux/users
 mkdir ./int-auto-recon/enum4linux/shares
-
-cd ./int-auto-recon/
 
 #Checking whether or not to start responder
 echo "Do you want to start Responder in a screen session?"
@@ -59,28 +60,30 @@ else
 	echo ""
 fi
 
+#	TODO: Add Ping scan option for large scopes
 
 #Nmap All TCP ports on all tagets
 	echo '[*] Initiating Full TCP port scan of all targets'
 	echo '[*] Timing updates provided every 120 seconds'
-	nmap -Pn --stats-every 120s --max-rtt-timeout 250ms --max-retries 3 --open --top-ports=65535 -oA ./nmap/fullscan -iL ./ips.txt | egrep '(remaining|Stats: )'
+	nmap -Pn --stats-every 120s --max-rtt-timeout 250ms --max-retries 3 --open --top-ports=65535 -oA ./int-auto-recon/nmap/fullscan -iL $1 | egrep '(remaining|Stats: )'
 	echo '[*] Full Scan Complete - Sorting Output'
-	cat ./nmap/fullscan.gnmap | grep open | cut -d " " -f 2 | grep -v Nmap > ./nmap/targets/listening_hosts.txt
+	cat ./int-auto-recon/nmap/fullscan.gnmap | grep open | cut -d " " -f 2 | grep -v Nmap > ./int-auto-recon/nmap/targets/listening_hosts.txt
 	echo '[*] Creating port file for next Nmap scan'
-	cat ./nmap/fullscan.gnmap | grep -v Status | grep -v Nmap | cut -d ':' -f 3 | sed "s|/open/tcp/||g" |cut -f 1 | sed 's|///|\n|g' | sed 's/ //g' | sed 's/,//g' | cut -d '/' -f 1 | sort -u | sed ':a;N;$!ba;s/\n/,/g' | sed 's/,//' > ./nmap/targets/portfile.txt
+	cat .int-auto-recon/nmap/fullscan.gnmap | grep -v Status | grep -v Nmap | cut -d ':' -f 3 | sed "s|/open/tcp/||g" |cut -f 1 | sed 's|///|\n|g' | sed 's/ //g' | sed 's/,//g' | cut -d '/' -f 1 | sort -u | sed ':a;N;$!ba;s/\n/,/g' | sed 's/,//' > ./nmap/targets/portfile.txt
 	echo '[*] Port file complete'
 	echo ""
 
 #Nmap Script/Service Scan only againt listening hosts/ports
-	ports=$(cat ./nmap/targets/portfile.txt)
+	ports=$(cat ./int-auto-recon/nmap/targets/portfile.txt)
 	echo '[*] Initiating Script and Service scan of open ports on all responding hosts'
 	echo "[*] Open ports: $ports"
 	echo '[*] Timing updates provided every 60 seconds'
-	nmap -Pn -sC -sV --open --stats-every 60s -oA ./nmap/script_service -iL ./nmap/targets/listening_hosts.txt -p $ports | egrep '(remaining|Stats: )'
+	nmap -Pn -sC -sV --open --stats-every 60s -oA ./nmap/script_service -iL ./int-auto-recon/nmap/targets/listening_hosts.txt -p $ports | egrep '(remaining|Stats: )'
 	echo '[*] Script/Service Scan Complete'
 	echo ""
-
+	
 #Sorting Nmap Outputs for common ports
+	cd ./int-auto-recon/
 	echo '[*] Sorting nmap output'
 	cat ./nmap/fullscan.gnmap | grep '21/open' | cut -d " " -f 2 > ./ports/ftp.txt
 	cat ./nmap/fullscan.gnmap | grep '22/open' | cut -d " " -f 2 > ./ports/ssh.txt
